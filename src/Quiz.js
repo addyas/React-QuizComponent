732935872e6a2a8b280eeaa28e94a4ae76bd4b90
adyas <ashleydyas92@gmail.com>
2019-06-05 14:40:49 -0400

    module5 cond

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,7 +11,7 @@ class Quiz extends Component {
 
   }
   render() {
-    const isQuizEnd = (this.state.quiz_position - 1 == quizData.quiz_questions.length) ? true : false;
+    const isQuizEnd = ((this.state.quiz_position - 1) == quizData.quiz_questions.length) ? true : false;
     return(
       <div> 
         {isQuizEnd && <QuizEnd />}
